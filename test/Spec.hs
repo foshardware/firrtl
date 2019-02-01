5d41556221097e736fa469390c86d2fae9c79564
Andreas Westerwick <westerwick@pconas.de>
2019-02-01 22:44:06 +0100

    tasty

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,13 +2,27 @@
 
 module Main where
 
+import Control.Exception (evaluate)
+
 import Data.FileEmbed
 import Data.Text.Encoding
+
+import Test.Tasty
+import Test.Tasty.HUnit
 
 import Language.FIRRTL.Lexer
 import Language.FIRRTL.Parser
 
 
 main :: IO ()
-main = putStrLn $ show $ circuit $ lexer [] $ decodeUtf8 $(embedFile "sample/test_harness.fir")
+main = defaultMain $ testGroup "FIR"
+  [ ansi_header
+  ]
 
+ansi_header :: TestTree
+ansi_header = testGroup "Rocket chip"
+  [ testCase "test_harness.fir" $ parse $ circuit $ lexer [] $ decodeUtf8 $(embedFile "sample/test_harness.fir")
+  ]
+
+parse :: Show a => a -> IO ()
+parse s = () <$ evaluate s
